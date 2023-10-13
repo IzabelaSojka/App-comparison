9e8fac37882273d646a87eaa5c7dd6600488226b
IzabelaSojka <sojkaizabela1@gmail.com>
2023-10-13 20:16:34 +0200

    Databases and Spring Data JPA

--- src/main/java/com/course/Main.java
+++ src/main/java/com/course/Main.java
@@ -14,55 +14,6 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    
-    @GetMapping("/greet")
-    public GreetResponse greet(){
-        GreetResponse response = new GreetResponse(
-                "Hello",
-                List.of("Java", "C++", "Typescript"),
-                new Person("Ann")
-                );
-        return response;
-    }
 
-    record Person(String name){
-
-    }
-    record GreetResponse(
-            String greet,
-            List<String> programmingLanguages,
-            Person person
-    ){}
-
-//    class GreetResponse{
-//        private final String greet;
-//
-//        GreetResponse(String greet){
-//            this.greet = greet;
-//        }
-//
-//        public String getGreet(){
-//            return greet;
-//        }
-//
-//        @Override
-//        public String toString(){
-//            return "GreetResponse{" +
-//                    "greet='" + greet + '\'' + '}';
-//        }
-//
-//        @Override
-//        public boolean equals(Object obj) {
-//            if(this == obj) return true;
-//            if(obj == null || getClass() != obj.getClass()) return false;
-//            GreetResponse that = (GreetResponse) obj;
-//            return Objects.equals(greet, that.greet);
-//        }
-//
-//        @Override
-//        public int hashCode(){
-//            return Objects.hash(greet);
-//        }
-//    }
 
 }
