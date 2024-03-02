ce22626b03fc99ec0c2ff5d01ce43aff30ab1661
IzabelaSojka <sojkaizabela1@gmail.com>
2024-03-02 19:26:20 +0100

    New context

--- src/main/java/com/course/customer/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.course.customer;
-
-import com.course.customer.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-}
